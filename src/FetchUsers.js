7ca0b9bf808e6e5fed14bb922362729446b873ce
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-24 07:53:47 -0700

    set useState for users data

--- src/FetchUsers.js
+++ src/FetchUsers.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const url = `https://randomuser.me/api`;
 
 const FetchUsers = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async() => {
+      const res = await fetch(url);
+      const users = await res.json();
+      setUsers(users.results);
+    }
+
+    fetchUserData();
+
+  }, [])
+  
+
   return (
     <>
     </>
