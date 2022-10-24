049d70be4c63697021e8100506290f3b7a1bc45c
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-24 09:32:19 -0700

    design user card

--- src/FetchUsers.js
+++ src/FetchUsers.js
@@ -1,33 +1,63 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 const url = `https://randomuser.me/api`;
 
 const FetchUsers = () => {
-
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const fetchUserData = async() => {
+    const fetchUserData = async () => {
       const res = await fetch(url);
       const users = await res.json();
       setUsers(users.results);
-    }
+    };
 
     fetchUserData();
-
-  }, [])
-  
+  }, []);
 
   return (
     <>
-    <section>
-      {users.map((user) => {
-        // destructuring
-      })}
-    </section>
+      <section>
+        {users.map((user) => {
+          // destructuring
+          const {
+            picture: { large },
+            name: { title, first, last },
+            gender,
+            location: { country },
+            dob: { date, age },
+          } = user;
+
+          return (
+            <div
+              key={user.login.uuid}
+              className="flex flex-col w-64 max-w-xs px-4 mt-6 p-4 bg-blue-900 rounded-xl"
+            >
+              <img src={large} alt="user img" className="rounded-3xl" />
+              <h1 className="truncate text-green-300">
+                <span className="font-bold">Name: </span>
+                {title}. {first} {last}
+              </h1>
+              <h1 className="truncate text-green-300">
+                <span className="font-bold">Gender:</span> {gender}
+              </h1>
+              <h1 className="truncate text-green-300">
+                <span className="font-bold">Age:</span> {age}
+              </h1>
+              <h1 className="truncate text-green-300">
+                <span className="font-bold">Origin:</span> {country}
+              </h1>
+              <h1 className="truncate text-green-300">
+                <span className="font-bold">DOB:</span>{" "}
+                {date}
+              </h1>
+            </div>
+          );
+        })}
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default FetchUsers;
